ae360d050f7594bd6ac4c5f47a14420f0468387e
Rômulo Jales <romulo@romulojales.com>
2015-08-05 18:51:00 -0300

    mais testes com json

--- src/json/json_test.go
+++ src/json/json_test.go
@@ -3,22 +3,52 @@ import (
 	"testing"
 	"encoding/json"
 	"bytes"
-	"fmt"
 )
 
-type QueryEsearch struct {
+type SimpleJson struct {
 	Sort   interface{} `json:"sort"`
 	Fields []string `json:"fields"`
+	Const  string `json:"const"`
 }
 
-func TestJson(t *testing.T) {
-	arr := QueryEsearch{Sort: []string{"issued", "fake"}, Fields: []string{"123", "321"}}
+type ComplexStruct struct {
+	Complex []SimpleJson `json:"complex"`
+}
+
+
+func TestMarshalSimpleJson(t *testing.T) {
+	arr := SimpleJson{Sort: []string{"issued", "fake"}, Fields: []string{"123", "321"}}
 	jsonb, _ := json.Marshal(arr)
 
-	ctrl := []byte(`{"sort":["issued","fake"],"fields":["123","321"]}`)
-	fmt.Println(string(jsonb))
-	fmt.Println(string(ctrl))
+	ctrl := []byte(`{"sort":["issued","fake"],"fields":["123","321"],"const":""}`)
+
 	if !bytes.Equal(jsonb, ctrl) {
-		t.Error("Json errado: %q", jsonb)
+		t.Error("Jsonb errado: %q", string(jsonb))
+		t.Error("ctrl errado: %q", string(ctrl))
 	}
+}
+
+func TestMarshalComplexJson(t *testing.T) {
+	ctrl := []byte(`{"complex":[{"sort":["issued","fake"],"fields":["123","321"],"const":"abc"}]}`)
+	arr := ComplexStruct{Complex:[]SimpleJson{SimpleJson{Sort: []string{"issued", "fake"}, Fields:
+	[]string{"123", "321"}, Const: "abc"}}}
+	jsonb, _ := json.Marshal(arr)
+
+	if !bytes.Equal(jsonb, ctrl) {
+		t.Error("Jsonb errado", string(jsonb))
+		t.Error("ctrl errado", string(ctrl))
+	}
+
+}
+
+
+func TestUnMarshalComplexJson(t *testing.T) {
+	ctrl := []byte(`{"complex":[{"sort":["issued","fake"],"fields":["123","321"],"const":"abc"}]}`)
+	arr := ComplexStruct{}
+	json.Unmarshal(ctrl, &arr)
+
+	if arr.Complex[0].Fields[0] != "123"{
+		t.Error("Jsonb errado", string(arr.Complex[0].Fields[0]))
+	}
+
 }
