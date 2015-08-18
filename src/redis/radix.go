0497db5a80af8b9a18c41a92008c2b8cf9c24413
Rômulo Jales <romulo@romulojales.com>
2015-08-18 16:04:30 -0300

    simples functions

--- src/redis/radix.go
+++ src/redis/radix.go
@@ -1,1 +1,24 @@
 package redis
+
+
+import "github.com/mediocregopher/radix.v2/sentinel"
+
+
+func GetSetinelClient() (*sentinel.Client, error) {
+	client, err := sentinel.NewClient("tcp", "localhost:26379", 100, "mymaster")
+	return client, err
+}
+
+func PingSentinel() (string, error){
+	client, err := GetSetinelClient()
+	if err != nil{
+		return "", err
+	}
+	c, err := client.GetMaster("mymaster")
+	if err != nil{
+		return "", err
+	}
+	resp := c.Cmd("Ping").String()
+	return resp, err
+}
+
