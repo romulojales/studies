7320a55b80c0dd297fb496ae944be08e72a652d2
Rômulo Jales <romulo@romulojales.com>
2017-02-27 15:53:12 -0300

    solvind 2d array

--- data structures/arrays/array_2d_ds.py
+++ data structures/arrays/array_2d_ds.py
@@ -8,6 +8,11 @@ for arr_i in range(6):
    arr.append(arr_t)
 
 
-for i in arr_t:
-    for j in arr_t[j]:
-        pass
+max_sum = float("-inf")
+for i in range(len(arr)-2):
+    for j in range(len(arr[i])-2):
+        local_sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j]+arr[i+2][j+1] + arr[i+2][j+2]
+        if local_sum > max_sum:
+            max_sum = local_sum
+
+print(max_sum)
