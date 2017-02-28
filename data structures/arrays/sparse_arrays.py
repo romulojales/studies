d24cf178d6cb5b563568a136ad5b5c32c1321836
Rômulo Jales <romulo@romulojales.com>
2017-02-27 22:05:58 -0300

    sparse arrays

---
+++ data structures/arrays/sparse_arrays.py
@@ -0,0 +1,13 @@
+number_of_strings = int(input().strip())
+strings = []
+for _ in range(number_of_strings):
+    strings.append(input().strip())
+number_of_queries = int(input().strip())
+
+for _ in range(number_of_queries):
+    query = input().strip()
+    occurs = 0
+    for string in strings:
+        if string == query:
+            occurs += 1
+    print(occurs)
