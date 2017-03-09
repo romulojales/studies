652aa2882a8d878f21a366f773ac6341fbed934e
Rômulo Jales <romulo.jales@corp.globo.com>
2017-03-09 15:23:12 -0300

    clean up code

--- data science from scratch/chap1.py
+++ data science from scratch/chap1.py
@@ -13,6 +13,23 @@ users = [{"id": 0, "name": "Hero"},
 
 friendships = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4), (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
 
+interests = [
+    (0, "Hadoop"), (0, "Big Data"), (0, "HBase"), (0, "Java"),
+    (0, "Spark"), (0, "Storm"), (0, "Cassandra"),
+    (1, "NoSQL"), (1, "MongoDB"), (1, "Cassandra"), (1, "HBase"),
+    (1, "Postgres"), (2, "Python"), (2, "scikit-learn"), (2, "scipy"),
+    (2, "numpy"), (2, "statsmodels"), (2, "pandas"), (3, "R"), (3, "Python"),
+    (3, "statistics"), (3, "regression"), (3, "probability"),
+    (4, "machine learning"), (4, "regression"), (4, "decision trees"),
+    (4, "libsvm"), (5, "Python"), (5, "R"), (5, "Java"), (5, "C++"),
+    (5, "Haskell"), (5, "programming languages"), (6, "statistics"),
+    (6, "probability"), (6, "mathematics"), (6, "theory"),
+    (7, "machine learning"), (7, "scikit-learn"), (7, "Mahout"),
+    (7, "neural networks"), (8, "neural networks"), (8, "deep learning"),
+    (8, "Big Data"), (8, "artificial intelligence"), (9, "Hadoop"),
+    (9, "Java"), (9, "MapReduce"), (9, "Big Data")
+]
+
 for user in users:
     user["friends"] = []
 
@@ -25,27 +42,6 @@ def number_of_friends(user):
     return len(user["friends"])
 
 
-total_connections = sum(number_of_friends(user) for user in users)
-
-print("Total Connections", total_connections)
-
-num_of_users = len(users)
-
-print("Number of users", num_of_users)
-
-avg_connections = total_connections / num_of_users
-
-print("Average of connections", avg_connections)
-
-num_of_friends_by_id = [(user["id"], number_of_friends(user)) for user in users]
-
-print("Number of friends by id", num_of_friends_by_id)
-
-sorted(num_of_friends_by_id, key=lambda x: x[1], reverse=True)
-
-print("Number of friends by id ordered", num_of_friends_by_id)
-
-
 def friends_of_friends(user):
     friends_of_friends = []
     for friend in user["friends"]:
@@ -54,9 +50,6 @@ def friends_of_friends(user):
             if user["id"] != fof and fof not in user["friends"]:
                 friends_of_friends.append(fof)
     return friends_of_friends
-
-
-print("Friends of Friends of user 0:", friends_of_friends(users[0]))
 
 
 def not_the_same(user, other_user):
@@ -70,5 +63,26 @@ def not_friends(user, other_user):
 def friends_of_friends_ids(user):
     return Counter(friends_of_friends(user))
 
+
+num_of_users = len(users)
+total_connections = sum(number_of_friends(user) for user in users)
+
+avg_connections = total_connections / num_of_users
+
+num_of_friends_by_id = [(user["id"], number_of_friends(user)) for user in users]
+
+sorted(num_of_friends_by_id, key=lambda x: x[1], reverse=True)
+
+print("Total Connections", total_connections)
+
+print("Number of users", num_of_users)
+
+print("Average of connections", avg_connections)
+
+print("Number of friends by id", num_of_friends_by_id)
+
+print("Number of friends by id ordered", num_of_friends_by_id)
+
+print("Friends of Friends of user 0:", friends_of_friends(users[0]))
 
 print(friends_of_friends_ids(users[3]))
