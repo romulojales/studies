c514c63676cc0ac20a03db940bf3fbc09b0e465d
Rômulo Jales <romulo.jales@corp.globo.com>
2017-03-09 15:21:22 -0300

    fof

--- data science from scratch/chap1.py
+++ data science from scratch/chap1.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 users = [{"id": 0, "name": "Hero"},
          {"id": 1, "name": "Dunn"},
          {"id": 2, "name": "Sue"},
@@ -15,7 +17,8 @@ for user in users:
     user["friends"] = []
 
 for i, j in friendships:
-    users[0]["friends"].append(users[j])
+    users[i]["friends"].append(j)
+    users[j]["friends"].append(i)
 
 
 def number_of_friends(user):
@@ -41,3 +44,31 @@ print("Number of friends by id", num_of_friends_by_id)
 sorted(num_of_friends_by_id, key=lambda x: x[1], reverse=True)
 
 print("Number of friends by id ordered", num_of_friends_by_id)
+
+
+def friends_of_friends(user):
+    friends_of_friends = []
+    for friend in user["friends"]:
+        friends_of_friend = users[friend]["friends"]
+        for fof in friends_of_friend:
+            if user["id"] != fof and fof not in user["friends"]:
+                friends_of_friends.append(fof)
+    return friends_of_friends
+
+
+print("Friends of Friends of user 0:", friends_of_friends(users[0]))
+
+
+def not_the_same(user, other_user):
+    return user["id"] != other_user["id"]
+
+
+def not_friends(user, other_user):
+    return other_user["id"] not in user["friends"]
+
+
+def friends_of_friends_ids(user):
+    return Counter(friends_of_friends(user))
+
+
+print(friends_of_friends_ids(users[3]))
